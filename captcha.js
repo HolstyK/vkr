440dfd141933da94a574d363c5a6278d07c71e91
HolstyK <145402755+HolstyK@users.noreply.github.com>
2023-10-16 23:14:42 +0300

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (userAnswer === captchaAnswer) {
       // Если пользователь правильно решил капчу, перенаправляем на основной сайт
-      window.location.href = 'index.html';
+      window.location.href = 'site.html';
     } else {
       alert('Неверный ответ. Попробуйте ещё раз.');
       // Генерируем новую капчу
