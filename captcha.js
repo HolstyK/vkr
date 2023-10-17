6427d6145f66f4710f68430a515966ce0dcfa0cf
HolstyK <145402755+HolstyK@users.noreply.github.com>
2023-10-17 16:28:02 +0300

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Функция для генерации капчи
-  function generateCaptcha() {
-    const num1 = Math.floor(Math.random() * 10);
-    const num2 = Math.floor(Math.random() * 10);
-    const operator = ['+', '-', '*'][Math.floor(Math.random() * 3)];
-
-    const question = `${num1} ${operator} ${num2}`;
-    const answer = eval(question);
-
-    document.getElementById('captcha-question').textContent = question;
-    return answer;
-  }
-
-  // Добавляем капчу к форме
-  let captchaAnswer = generateCaptcha();
-  const captchaAnswerInput = document.getElementById('captcha-answer');
-  const captchaSubmitButton = document.getElementById('captcha-submit');
-
-  captchaSubmitButton.addEventListener('click', function () {
-    const userAnswer = parseInt(captchaAnswerInput.value, 10);
-
-    if (userAnswer === captchaAnswer) {
-      // Если пользователь правильно решил капчу, перенаправляем на основной сайт
-      window.location.href = 'site.html';
-    } else {
-      alert('Неверный ответ. Попробуйте ещё раз.');
-      // Генерируем новую капчу
-      captchaAnswer = generateCaptcha();
-      captchaAnswerInput.value = '';
+    // Функция для генерации капчи
+    function generateCaptcha() {
+      const num1 = Math.floor(Math.random() * 10);
+      const num2 = Math.floor(Math.random() * 10);
+      const operator = ['+', '-', '*'][Math.floor(Math.random() * 3)];
+  
+      const question = `${num1} ${operator} ${num2}`;
+      const answer = eval(question);
+  
+      document.getElementById('captcha-question').textContent = question;
+      return answer;
     }
+  
+    // Добавляем капчу к форме
+    let captchaAnswer = generateCaptcha();
+    const captchaAnswerInput = document.getElementById('captcha-answer');
+    const captchaSubmitButton = document.getElementById('captcha-submit');
+  
+    captchaSubmitButton.addEventListener('click', function () {
+      const userAnswer = parseInt(captchaAnswerInput.value, 10);
+  
+      if (userAnswer === captchaAnswer) {
+        // Если пользователь правильно решил капчу, перенаправляем на основной сайт
+        window.location.href = 'https://www.ozon.ru/';
+      } else {
+        alert('Неверный ответ. Попробуйте ещё раз.');
+        // Генерируем новую капчу
+        captchaAnswer = generateCaptcha();
+        captchaAnswerInput.value = '';
+      }
+    });
   });
-});
