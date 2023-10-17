e4d594fa084dee45137575644e039c587d8ac995
HolstyK <145402755+HolstyK@users.noreply.github.com>
2023-10-17 16:28:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (isMobile) {
         // Если пользователь на мобильном устройстве, добавляем ссылку на Play Market
-        appLink.innerHTML = '<a href="https://play.google.com/store/apps/details?id=your-app-package">Скачать на Play Market</a>';
+        appLink.innerHTML = '<a href="captсha.html">Скачать на Play Market</a>';
     } else {
         // Если пользователь на ПК или другом устройстве, добавляем простую ссылку на скачивание
-        appLink.innerHTML = '<a href="https://www.ozon.ru/">Скачать приложение</a>';
+        appLink.innerHTML = '<a href="captсha.html">Скачать приложение</a>';
     }
 });
